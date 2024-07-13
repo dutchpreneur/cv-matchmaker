2820352cb107f7c9963fb95b76064174f129a575
dutchpreneur <144556219+dutchpreneur@users.noreply.github.com>
2024-07-13 21:13:48 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.menu-icon').addEventListener('click', function() {
+        document.getElementById('menu').classList.toggle('show');
+    });
+});
